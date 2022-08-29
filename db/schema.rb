c2d7f5edfdee5d57aee39207e61dffe61f625db9
Francisco Charris <franciscocharrisc@gmail.com>
2022-08-29 15:37:17 -0500

    channels migration runned

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_202819) do
+ActiveRecord::Schema.define(version: 2022_08_29_203115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "channels", force: :cascade do |t|
+    t.string "channel"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "discussions", force: :cascade do |t|
     t.string "tittle"
