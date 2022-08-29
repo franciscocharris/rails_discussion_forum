82c9f1f6af241b21000b81d35d95bd56827f611e
Francisco Charris <franciscocharrisc@gmail.com>
2022-08-29 15:47:45 -0500

    add_channel_id_to_discussions migration runned

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_203957) do
+ActiveRecord::Schema.define(version: 2022_08_29_204332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2022_08_29_203957) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.bigint "channel_id", null: false
+    t.index ["channel_id"], name: "index_discussions_on_channel_id"
     t.index ["user_id"], name: "index_discussions_on_user_id"
   end
 
@@ -53,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_08_29_203957) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "discussions", "channels"
   add_foreign_key "discussions", "users"
   add_foreign_key "replies", "discussions"
   add_foreign_key "replies", "users"
